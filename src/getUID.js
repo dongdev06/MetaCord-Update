7fcc5aeb7a994d80ca30db987d20ccd56f510fbc
Shinchan0911 <truonghuuloc212@gmail.com>
2023-10-29 16:24:39 +0700

    1,0,7

--- src/getUID.js
+++ src/getUID.js
@@ -19,36 +19,34 @@ module.exports = function (_defaultFuncs, api, _ctx) {
       
     try {
         var Link = String(link);
-        var FindUID = require('../utils/Extension');
+        var FindUID = require('../utils/Extension').getUID;
         if (Link.includes('facebook.com') || Link.includes('Facebook.com') || Link.includes('fb')) {
             var LinkSplit = Link.split('/');
             if (LinkSplit.indexOf("https:") == 0) {
               if (!isNaN(LinkSplit[3]) && !Link.split('=')[1]  && !isNaN(Link.split('=')[1])) {
-                api.sendMessage('Wrong link, link should be formatted as follows: facebook.com/Lazic.Kanzu',global.Fca.Data.event.threadID,global.Fca.Data.event.messageID);
-                callback(null, String(4));
+                callback(null, 'Wrong link, link should be formatted as follows: facebook.com/Lazic.Kanzu');
               }
               else if (!isNaN(Link.split('=')[1]) && Link.split('=')[1]) {
                 var Format = `https://www.facebook.com/profile.php?id=${Link.split('=')[1]}`;
-                FindUID(Format,api).then(function (data) {
+                FindUID(Format, (data) => {
                   callback(null, data);
                 });
               } 
               else {
-                FindUID(Link,api).then(function (data) {
+                FindUID(Link, (data) => {
                   callback(null, data);
                 });
               }
             }
             else {
                 var Form = `https://www.facebook.com/${LinkSplit[1]}`;
-                FindUID(Form,api).then(function (data) {
-                    callback(null, data);
+                FindUID(Form, (data) => {
+                  callback(null, data);
                 });
             }
         }
         else {
-            callback(null, null);
-            api.sendMessage('Wrong link, link needs to be Facebook',global.Fca.Data.event.threadID,global.Fca.Data.event.messageID);
+            callback(null, 'Wrong link, link needs to be Facebook');
         }
     }
     catch (e) {
--- utils/Extension.js
+++ utils/Extension.js
@@ -7,28 +7,23 @@ const chalk = require('chalk');
 
 function Uptime(url) {
     switch (process.platform) {
-        case 'win32': {
-          logger("Your Operating System does not currently support Auto Uptime !");
-          break;
-        }
-        case 'darwin': {
-          logger("Your Operating System does not currently support Auto Uptime !");
-          break;
-        }
-        case 'linux':
-          if (process.env.REPL_SLUG) {
-            logger(`Activating uptime for ${chalk.underline(`'${url}'`)}`);
-            return setInterval(function () {
-              axios.get(url).then(() => { }).catch(() => { });
-            }, 10 * 1000);
-          }
-          else {
+        case 'win32':
+        case 'darwin':
             logger("Your Operating System does not currently support Auto Uptime !");
-          }
-          break;
+            break;
+        case 'linux':
+            if (process.env.REPL_SLUG) {
+                logger(`Activating uptime for ${chalk.underline(`'${url}'`)}`);
+                return setInterval(function () {
+                    axios.get(url).then(() => { }).catch(() => { });
+                }, 10 * 1000);
+            } else {
+                logger("Your Operating System does not currently support Auto Uptime !");
+            }
+            break;
         default:
-          logger("Your Operating System does not currently support Auto Uptime !");
-      }
+            logger("Your Operating System does not currently support Auto Uptime !");
+    }
 }
 
 function CreateSiteHtml(port) {
@@ -36,69 +31,80 @@ function CreateSiteHtml(port) {
 
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, '/Html/index.html'));
-      });
-      
+    });
+
     app.get('/', (req, res) => res.sendStatus(200));
 
     const listen_port = port || 3000;
 
     app.listen(listen_port, () => {
         logger(`Bot is running on port: ${listen_port}`);
-       
+    });
+
     app.on('error', (error) => {
         logger(`An error occurred while starting the server: ${error}`);
-      });
     });
 }
 
-async function getUIDSlow(url,api) {
-    var FormData =  require("form-data");
+async function getUIDSlow(url) {
+    var FormData = require("form-data");
     var Form = new FormData();
-	var Url = new URL(url);
+    var Url = new URL(url);
     Form.append('username', Url.pathname.replace(/\//g, ""));
-	try {
-        var data = await got.post('https://api.findids.net/api/get-uid-from-username',{
-            body: Form,
-            userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.79 Safari/537.36'
-        })
-	} catch (e) {
-        console.log(global.Fca.Data.event.threadID,e)
-        return api.sendMessage("Lỗi: " + e.message,global.Fca.Data.event.threadID);
-	}
-    if (JSON.parse(data.body.toString()).status != 200) return api.sendMessage('Error !',global.Fca.Data.event.threadID)
-    if (typeof JSON.parse(data.body.toString()).error === 'string') return "errr"
-    else return JSON.parse(data.body.toString()).data.id || "Not Found";
+    try {
+        var response = await axios.post('https://api.findids.net/api/get-uid-from-username', Form, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.79 Safari/537.36'
+            }
+        });
+        var data = response.data;
+        if (data.status !== 200) {
+            return console.log('Error!');
+        }
+        if (typeof data.error === 'string') {
+            return "errr";
+        } else {
+            return data.data.id || "Not Found";
+        }
+    } catch (e) {
+        return console.log("Lỗi: " + e.message);
+    }
 }
 
-async function getUIDFast(url,api) {
-    var FormData =  require("form-data");
+async function getUIDFast(url) {
+    var FormData = require("form-data");
     var Form = new FormData();
-	var Url = new URL(url);
+    var Url = new URL(url);
     Form.append('link', Url.href);
     try {
-        var data = await got.post('https://id.traodoisub.com/api.php',{
-            body: Form
-        })
-	} catch (e) {
-        return api.sendMessage("Lỗi: " + e.message,global.Fca.Data.event.threadID,global.Fca.Data.event.messageID);
-	}
-    if (JSON.parse(data.body.toString()).error) return api.sendMessage(JSON.parse(data.body.toString()).error,global.Fca.Data.event.threadID,global.Fca.Data.event.messageID);
-    else return JSON.parse(data.body.toString()).id || "Not Found";
+        var response = await axios.post('https://id.traodoisub.com/api.php', Form);
+        var data = response.data;
+        if (data.error) {
+            return console.log(data.error);
+        } else {
+            return data.id || "Not Found";
+        }
+    } catch (e) {
+        return console.log("Lỗi: " + e.message);
+    }
 }
 
-async function getUID(url,api) {
-    var getUID = await getUIDFast(url,api);
-        if (!isNaN(getUID) == true) return getUID;  
-            else {
-                let getUID = await getUIDSlow(url,api);
-            if (!isNaN(data) == true) return getUID;
-        else return null;
-    }
-}       
+async function getUID(url, callback) {
+  var getUID = await getUIDFast(url);
+  if (!isNaN(getUID)) {
+      callback(getUID);
+  } else {
+      let getUIDSlowResult = await getUIDSlow(url);
+      if (!isNaN(getUIDSlowResult)) {
+          callback(getUIDSlowResult);
+      } else {
+          callback(null);
+      }
+  }
+}
 
 module.exports = {
     CreateSiteHtml,
     Uptime,
     getUID
 };
-  
